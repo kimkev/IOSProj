9b785211ffc25789dd633c105d62536096caa116
Xcode User <xcode@fasts-macbook-pro-4.local>
2019-03-16 21:59:40 -0400

    Initial Commit

---
+++ AR_Racing_Revolution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR_Racing_Revolution
+//
+//  Created by Xcode User on 2019-03-16.
+//  Copyright © 2019 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
