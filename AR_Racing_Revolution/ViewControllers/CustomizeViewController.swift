e4ecaffdeaa0d668d743d71961cd44746b03bb14
Xcode User <xcode@Fasts-MacBook-Pro-4.local>
2019-03-16 23:38:17 -0400

    Second Commit

---
+++ AR_Racing_Revolution/ViewControllers/CustomizeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomizeViewController.swift
+//  AR_Racing_Revolution
+//
+//  Created by Xcode User on 2019-03-16.
+//  Copyright © 2019 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class CustomizeViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
