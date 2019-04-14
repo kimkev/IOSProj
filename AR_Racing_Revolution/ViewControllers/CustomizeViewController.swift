af1aa2bcbf749d705d9ab271803adffa47ed503f
Trevor Rubie <rubiet@sheridancollege.ca>
2019-04-14 11:46:02 -0400

    finished customize view

--- AR_Racing_Revolution/ViewControllers/CustomizeViewController.swift
+++ AR_Racing_Revolution/ViewControllers/CustomizeViewController.swift
@@ -5,12 +5,58 @@
 //  Created by Xcode User on 2019-03-16.
 //  Copyright © 2019 Xcode User. All rights reserved.
 //
-// By: Kevin Kim
+// Allows for a preview of what the players car will look like in the ARView
+// By: Trevor Rubie
 
 import UIKit
 
 class CustomizeViewController: ViewController {
 
+    @IBOutlet var sgCar : UISegmentedControl!
+    @IBOutlet var sgTire : UISegmentedControl!
+    
+    @IBOutlet var imCar : UIImageView!
+    @IBOutlet var imTire : UIImageView!
+    
+    //Changes tire size when seg changes
+    func updateTire(){
+        
+        let tire = sgTire.selectedSegmentIndex
+        
+        if tire == 0 {
+            imTire.image = UIImage(named: "smalltire")
+        } else if tire == 1{
+            imTire.image = UIImage(named: "medtire")
+        } else {
+           imTire.image = UIImage(named: "bigtire")
+        }
+        
+        
+    }
+    //Changes car colour when seg changes
+    func updateCar(){
+        
+        let car = sgCar.selectedSegmentIndex
+        
+        if car == 0 {
+            imCar.image = UIImage(named: "greencar")
+        } else if car == 1 {
+            imCar.image = UIImage(named: "bluecar")
+        } else if car == 2 {
+            imCar.image = UIImage(named: "yellowcar")
+        } else {
+            imCar.image = UIImage(named: "redcar")
+        }
+        
+        
+    }
+    
+    //Listener for segment change
+    @IBAction func segDidChange(sender : UISegmentedControl){
+        updateCar()
+        updateTire()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
