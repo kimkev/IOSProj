c87cc2afa37533444c205063608dc5f6e746ed9a
Xcode User <xcode@Fasts-MacBook-Pro-4.local>
2019-04-15 11:29:10 -0400

    fix emailer

--- AR_Racing_Revolution/ViewControllers/EmailViewController.swift
+++ AR_Racing_Revolution/ViewControllers/EmailViewController.swift
@@ -30,32 +30,44 @@ class EmailViewController: UIViewController, MFMailComposeViewControllerDelegate
     }
     
     
+    //     hide keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // email sending method
     @IBAction func sendEmail(sender : Any){
-        let mailComposeVC = configureMailController()
+        //        let mailComposeVC = configureMailController()
         
-        if (!MFMailComposeViewController.canSendMail()) {
-            showMailError()
-            return
+        if let mailComposeVC : MFMailComposeViewController = MFMailComposeViewController() {
+            mailComposeVC.mailComposeDelegate = self
+            mailComposeVC.setToRecipients([tfEmail.text!])
+            mailComposeVC.setSubject(tfTitle.text!)
+            mailComposeVC.setMessageBody(tvBody.text!, isHTML: false)
+            self.present(mailComposeVC, animated: true, completion: nil)
         }
         else{
-           self.present(mailComposeVC, animated: true, completion: nil)
+            
+            showMailError()
         }
     }
     
     
-    func configureMailController() -> MFMailComposeViewController {
-        let composeVC = MFMailComposeViewController()
-        composeVC.mailComposeDelegate = self
-        // Configure the fields of the interface.
-        composeVC.setToRecipients([tfEmail.text!])
-        composeVC.setSubject(tfTitle.text!)
-        composeVC.setMessageBody(tvBody.text!, isHTML: false)
-        // Present the view controller modally.
-        self.present(composeVC, animated: true, completion: nil)
-        
-        return composeVC
-    }
+    //    func configureMailController() -> MFMailComposeViewController {
+    //        let composeVC = MFMailComposeViewController()
+    //        composeVC.mailComposeDelegate = self
+    //        // Configure the fields of the interface.
+    //        composeVC.setToRecipients([tfEmail.text!])
+    //        composeVC.setSubject(tfTitle.text!)
+    //        composeVC.setMessageBody(tvBody.text!, isHTML: false)
+    //        // Present the view controller modally.
+    //        self.present(composeVC, animated: true, completion: nil)
+    //
+    //        return composeVC
+    //    }
     
+    // show error as a popup if email doesn't work
     func showMailError(){
         let sendMailErrorAlert = UIAlertController(title: "could not send email", message: "Device could not send email", preferredStyle: .alert)
         let dismiss = UIAlertAction(title: "Ok", style: .default, handler: nil)
@@ -67,24 +79,24 @@ class EmailViewController: UIViewController, MFMailComposeViewControllerDelegate
         controller.dismiss(animated: true, completion: nil)
     }
     
-
     
+    // set the email textfield from the all players table
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tfEmail.text = email
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
